1e26f13799c7bb9012229622823b9e0c7bc09e06
mrokicki <mrokicki@unity-t.pl>
2017-09-30 22:11:06 +0200

    sprites

--- script/GameRenderer.js
+++ script/GameRenderer.js
@@ -3,6 +3,21 @@ function GameRenderer(sprites) {
 }
 
 GameRenderer.prototype.render = function(worldMap, view, context) {
-    console.log(worldMap);
+    //console.log(worldMap);
+    //console.log(view);
+    //var x,y;
+
+    /*for (y=view.y; y<view.y+view.height; ++y) {
+        for (x=view.x; x<view.x+view.width; ++x) {
+
+        }
+    }*/
+
+    this.__renderBg(view, context);
+};
+
+GameRenderer.prototype.__renderBg = function(view, context) {
+    context.fillStyle = 'green';
     console.log(view);
+    context.fillRect(0,0,view.width,view.height);
 };
--- script/GameStarter.js
+++ script/GameStarter.js
@@ -8,12 +8,12 @@ GameStarter.setWorld = function(word) {
 GameStarter.onLoad = function(canvasId) {
     var canvas = $("#"+canvasId);
 
-    var view = new GameView(0, 0, canvas.width, canvas.height)
+    var view = new GameView(0, 0, canvas.width(), canvas.height())
     var sprites = new Sprites();
     var renderer = new GameRenderer(sprites);
 
     var ctx = canvas[0].getContext("2d");
     renderer.render(GameStarter.__word.map, view, ctx);
 
-    alert('AAAAAAAA');
+    //alert('AAAAAAAA');
 };
