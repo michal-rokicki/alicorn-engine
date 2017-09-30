b98b6f48512a02f59df99c318d74f0d2e956bd1a
mrokicki <mrokicki@unity-t.pl>
2017-09-30 23:24:05 +0200

    clock

--- script/GameStarter.js
+++ script/GameStarter.js
@@ -7,18 +7,7 @@ GameStarter.setWorld = function(word) {
 
 GameStarter.onLoad = function(canvasId) {
     GameStarter.__gamePanel = new GamePanel($("#"+canvasId), GameStarter.__word);
-
     GameStarter.nextGameStep();
-
-    //timeout()
-    /*var canvas = $("#"+canvasId);
-
-    var view = new GameView(0, 0, canvas.width(), canvas.height())
-    var sprites = new Sprites();
-    var renderer = new GameRenderer(sprites);
-
-    var ctx = canvas[0].getContext("2d");
-    renderer.render(GameStarter.__word, view, ctx);*/
 };
 
 GameStarter.nextGameStep = function() {
