42fdfb5e9e21877b462b976563da151f6e9b7d16
mrokicki <mrokicki@unity-t.pl>
2017-09-30 23:17:44 +0200

    main character

---
+++ script/GamePanel.js
@@ -0,0 +1,11 @@
+function GamePanel(canvas, world) {
+    this.__word = world;
+    this.__canvas = canvas;
+    this.__view = new GameView(0, 0, canvas.width(), canvas.height())
+    this.__renderer = new GameRenderer(new Sprites());
+}
+
+GamePanel.prototype.redraw = function() {
+    var ctx = this.__canvas[0].getContext("2d");
+    this.__renderer.render(this.__word, this.__view, ctx);
+};
--- script/GameStarter.js
+++ script/GameStarter.js
@@ -6,12 +6,14 @@ GameStarter.setWorld = function(word) {
 };
 
 GameStarter.onLoad = function(canvasId) {
-    var canvas = $("#"+canvasId);
+    var gamePanel = new GamePanel($("#"+canvasId), GameStarter.__word);
+    gamePanel.redraw();
+    /*var canvas = $("#"+canvasId);
 
     var view = new GameView(0, 0, canvas.width(), canvas.height())
     var sprites = new Sprites();
     var renderer = new GameRenderer(sprites);
 
     var ctx = canvas[0].getContext("2d");
-    renderer.render(GameStarter.__word, view, ctx);
+    renderer.render(GameStarter.__word, view, ctx);*/
 };
