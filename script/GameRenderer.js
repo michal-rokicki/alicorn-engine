de9434305938637f2a6a0e87cddfac15284faa3b
mrokicki <mrokicki@unity-t.pl>
2017-10-01 00:52:05 +0200

    gravity

--- script/GameRenderer.js
+++ script/GameRenderer.js
@@ -19,11 +19,11 @@ GameRenderer.prototype.__renderBg = function(view, context) {
 GameRenderer.prototype.__renderBlocks = function(worldMap, view, context) {
     var i,j, x, y;
 
-    var i0 = Math.max(0, Math.floor(view.x/GameRenderer.__BLOCK_SIZE));
-    var i1 = Math.min(worldMap.width, Math.floor((view.x+view.width)/GameRenderer.__BLOCK_SIZE));
+    var i0 = Math.max(0, Math.floor(view.x/GameRenderer.__BLOCK_SIZE)-1);
+    var i1 = Math.min(worldMap.width, Math.floor((view.x+view.width)/GameRenderer.__BLOCK_SIZE)+1);
 
-    var j0 = Math.max(0, Math.floor(view.y/GameRenderer.__BLOCK_SIZE));
-    var j1 = Math.min(worldMap.height, Math.floor((view.y+view.height)/GameRenderer.__BLOCK_SIZE));
+    var j0 = Math.max(0, Math.floor(view.y/GameRenderer.__BLOCK_SIZE)-1);
+    var j1 = Math.min(worldMap.height, Math.floor((view.y+view.height)/GameRenderer.__BLOCK_SIZE)+1);
 
     for (i=i0; i<i1; ++i) {
         for (j=j0; j<j1; ++j) {
