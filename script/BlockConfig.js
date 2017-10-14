1b749b45bcc67617d5b0c8bfdfb8a23f49b76988
michal.rokicki <michal.rokicki@gmail.com>
2017-10-14 19:45:29 +0200

    woda

--- script/BlockConfig.js
+++ script/BlockConfig.js
@@ -4,6 +4,7 @@ function BlockConfig() {
         "1": "SOLID",
         "2": "SOLID",
         "3": "SOLID",
+        "4": "SOLID",
         "a": "SOLID",
         "X": "SOLID"
     }
--- script/Sprites.js
+++ script/Sprites.js
@@ -3,6 +3,7 @@ function Sprites() {
         "1": document.getElementById("block-1-img"),
         "2": document.getElementById("block-2-img"),
         "3": document.getElementById("block-3-img"),
+        "4": document.getElementById("block-4-img"),
         "a": document.getElementById("block-a-img"),
         "X": document.getElementById("block-X-img")
     };
